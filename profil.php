79a615214646a5631f069737c23d7c96f37c6af5
Chay <poumba@MacBook-Pro-de-Chay.local>
2020-11-17 11:48:23 +0100

    debut de page index

--- profil.php
+++ profil.php
@@ -2,33 +2,48 @@
 session_start();
 
 $bdd = mysqli_connect("localhost", "root", "root", "moduleconnexion"); // Connexion database...
-
 if(isset($_SESSION["id"]))
 {
+    if(isset($_POST['newlogin']) AND !empty($_POST['newlogin'])) 
+    {
+        $newlogin = htmlspecialchars($_POST["newlogin"]);
+
+        $sql = "UPDATE utilisateurs SET login = '$newlogin' WHERE id = '".$_SESSION["id"]."'";
+        $result = mysqli_query($bdd,$sql) or die(mysqli_error($bdd));
+    }
+
+    if(isset($_POST['newprenom']) AND !empty($_POST['newprenom'])) 
+    {
+        $newprenom = htmlspecialchars($_POST["newprenom"]);
+
+        $sql = "UPDATE utilisateurs SET prenom = '$newprenom' WHERE id = '".$_SESSION["id"]."'";
+        $result = mysqli_query($bdd,$sql) or die(mysqli_error($bdd));
+
+    }
+
+    if(isset($_POST['newnom']) AND !empty($_POST['newnom'])) 
+    {
+        $newnom = htmlspecialchars($_POST["newnom"]);
+
+        $sql = "UPDATE utilisateurs SET nom = '$newnom' WHERE id = '".$_SESSION["id"]."'";
+        $result = mysqli_query($bdd,$sql) or die(mysqli_error($bdd));
+    }
+    if(isset($_POST['newpassword']) AND !empty($_POST['newpassword']) ) 
+    {
+        $newpassword = htmlspecialchars($_POST["newpassword"]);
+
+        $sql = "UPDATE utilisateurs SET password = '$newpassword' WHERE id = '".$_SESSION["id"]."'";
+        $result = mysqli_query($bdd,$sql) or die(mysqli_error($bdd));
+    }
+     
 
     $sql = "SELECT * FROM utilisateurs WHERE id = '".$_SESSION["id"]."'";  // Recovery User session ...
     $result = mysqli_query($bdd,$sql) or die(mysqli_error($bdd));
     $userinfo = mysqli_fetch_array($result);
+
+
+    mysqli_close($bdd);
 }
-
-    $newlogin = "";
-    $newprenom  = "";
-    $newnom = "";
-    $newpassword  = "";
-
-if(isset($_POST) && isset($_POST["newlogin"]) && isset($_POST["newprenom"])  && isset($_POST["newnom"])  && isset($_POST["newpassword"]) )
-{
-    $newlogin = htmlspecialchars($_POST["newlogin"]);
-    $newprenom = htmlspecialchars($_POST["newprenom"]);
-    $newnom = htmlspecialchars($_POST["newnom"]);
-    $newpassword = htmlspecialchars($_POST["newpassword"]);
-
-    $sql = "UPDATE utilisateurs SET login = '$newlogin', prenom ='$newprenom', nom ='$newnom', password ='$newpassword'  WHERE id = '".$_SESSION["id"]."';";
-    $result = mysqli_query($bdd,$sql) or die(mysqli_error($bdd));
-    $message = "Record Modified Successfully"; 
-}
-
-
 
 ?>
 
@@ -54,17 +69,17 @@ if(isset($_POST) && isset($_POST["newlogin"]) && isset($_POST["newprenom"])  && 
             <section>
                 <!--Debut form -->
                 <form method="post" action="profil.php">
-                    <label for="login">login</label>
+                    <label for="login">Login</label>
                     <input type="text" name="newlogin" id="login" placeholder="votre login"  value="<?php echo $userinfo["login"]?>">
 
-                    <label for="prenom">prenom</label>
+                    <label for="prenom">Prenom</label>
                     <input type="text" name="newprenom" id="prenom" placeholder="Votre prenom"  value="<?php echo $userinfo["prenom"]?>">
 
-                    <label for="nom">nom</label>
+                    <label for="nom">Nom</label>
                     <input type="text" name="newnom" id="nom" placeholder="Votre nom"  value="<?php echo $userinfo["nom"]?>">
 
                     <label for="password">Mot de passe</label>
-                    <input type="password" name="newpassword" id="password" placeholder="Votre mot de passe">
+                    <input type="password" name="newpassword" id="password" placeholder="Nouveau mot de passe">
 
                     <input type="submit" name="submit" value="Modifier son profile">
                 </form>
@@ -72,9 +87,7 @@ if(isset($_POST) && isset($_POST["newlogin"]) && isset($_POST["newprenom"])  && 
             </section>
         </article>
     </main>
-    <?php
-            echo $message;
-    ?>
+
     <footer>
         <p>Copyright 2020 </p>
     </footer>
